eec743431e5dad2ccac437db51fc443190ddc06b
Tal Cohen <talcohen318@gmail.com>
2023-06-12 12:53:58 +0300

    API keys removed code refactoring for better Code Efficiency more

--- spotify_funcs.py
+++ spotify_funcs.py
@@ -2,12 +2,12 @@ import re
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import requests
+from artist import Artist
+import APIs
 
-#spotify api:
-your_client_id = '47b7fe5aad594079b5c0d9d4c62819c2'
-your_client_secret = '9979cf4e8f524bcdb1702816b79d2988'
-
-
+# Replace with your own Spotify API credentials
+client_id = APIs.spotify_client_id_API
+client_secret = APIs.spotify_client_secret_API
 
 
 def cut_content_after_question_mark(link):
@@ -23,13 +23,7 @@ def cut_content_after_question_mark(link):
         return link
 
 
-
-
 def get_artists_from_spotify(playlist_link):
-    # Replace with your own Spotify API credentials
-    client_id = your_client_id
-    client_secret = your_client_secret
-
     # Authenticate with Spotify API
     client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -40,19 +34,33 @@ def get_artists_from_spotify(playlist_link):
     results = sp.playlist_items(playlist_id)
     tracks = results['items']
 
-    # Iterate over the tracks and extract artists' names
-    artists = []
+    # Retrieve all tracks in the playlist
     while results['next']:
         results = sp.next(results)
         tracks.extend(results['items'])
 
+    artists = []
     for track in tracks:
         for artist in track['track']['artists']:
-            if artist['name'] not in artists:
-                artists.append(artist['name'])
-                # print(artist['name'])
+            artist_name = artist['name']
+            if is_artist_in_array(artist_name, artists):
+                find_artist_and_update_songs_num(artist_name, artists)
+            else:
+                new_artist = Artist(artist_name, 'none', 'none', 'none')
+                artists.append(new_artist)
+
     return artists
 
+
+def find_artist_and_update_songs_num(artist_name, artists):
+    for artist in artists:
+        if artist.name == artist_name:
+            artist.songs_num += 1
+            break
+
+
+def is_artist_in_array(artist_name, array):
+    return any(artist.name == artist_name for artist in array)
 
 
 def is_link_good(link):
@@ -70,17 +78,10 @@ def is_link_good(link):
     if not link:
         return False
 
-    # Try to access the link.
     try:
         response = requests.get(link)
-    except requests.exceptions.HTTPError as e:
-        # The link is not valid.
-        return False
-
-    # Check if the request was successful.
-    if response.status_code == 200:
-        # The link is valid.
-        return True
-    else:
+        # Check if the request was successful.
+        return response.status_code == 200
+    except requests.exceptions.HTTPError:
         # The link is not valid.
         return False
