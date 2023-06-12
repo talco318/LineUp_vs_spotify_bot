71cc32c0ac99d905c31ec52422518a40bbf2e7c4
Tal Cohen <talcohen318@gmail.com>
2023-06-12 12:52:51 +0300

    artist.py file refactored after show.py was added

--- artist.py
+++ artist.py
@@ -1,9 +1,22 @@
+from show import Show
 class Artist:
+    def __init__(self, name,songs_num, host_name_and_stage, weekend, date):
+        self.name = name
+        self.songs_num = songs_num
+        self.show = Show(weekend, host_name_and_stage, date)
+        self.show2 = None
+
     def __init__(self, name, host_name_and_stage, weekend, date):
         self.name = name
-        self.host_name_and_stage = host_name_and_stage
-        self.weekend = weekend
-        self.date = date
+        self.songs_num = 1
+        self.show = Show(weekend, host_name_and_stage, date)
+        self.show2 = None
+
+    def add_show(self,weekend, host_name_and_stage, date):
+        self.show2 = Show(weekend,host_name_and_stage,date)
 
     def __str__(self):
-        return f"*{self.name}*\nStage and host name: {self.host_name_and_stage}\nWeekend:\n{self.weekend}\nDate:\n{self.date}"
+        output = f"*{self.name}*- Songs number: {self.songs_num}\nShow:\n{self.show}"
+        if self.show2 is not None:
+            output += f"\nShow2:\n{self.show2}"
+        return output
