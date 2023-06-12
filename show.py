003c5a8599b4821d5a6b205306811aabb0872db2
Tal Cohen <talcohen318@gmail.com>
2023-06-12 12:52:24 +0300

    show.py class was added for better arrange of artists calss

---
+++ show.py
@@ -0,0 +1,8 @@
+class Show:
+    def __init__(self, weekend_number,host_name_and_stage, date):
+        self.weekend_number = weekend_number
+        self.host_name_and_stage = host_name_and_stage
+        self.date = date
+
+    def __str__(self):
+        return f"*{self.weekend_number}*\nStage and host name: {self.host_name_and_stage}\nDate: {self.date}"
