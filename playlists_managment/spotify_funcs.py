d0ae9f010d848476c6a089d174609a108b4745b4
tal <talcohen318@gmail.com>
2024-06-19 09:39:04 +0300

    better code docstrings, readability, structure, and performance

--- playlists_managment/spotify_funcs.py
+++ playlists_managment/spotify_funcs.py
@@ -1,9 +1,8 @@
 # ./playlists_managment/spotify_funcs.py
-
 import re
+from typing import List
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
 import APIs
 from app.artist import Artist
 
@@ -16,43 +15,38 @@ client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-def cut_content_after_question_mark(link):
+def cut_content_after_question_mark(link: str) -> str:
     """
     Cuts the content after the question mark in a given link.
 
-    Arguments:
+    Args:
         link (str): A string representing a link with or without a question mark.
 
     Returns:
         str: The content before the question mark, if it exists, otherwise the original link.
     """
-    # Use regular expression to match the question mark and the content after it
     match = re.search(r"^(.*?)(\?.*)$", link)
-
     # If the match is found, return the content before the question mark
     if match:
         return match.group(1)
-
     # Otherwise, return the original link
     return link
 
 
-def get_artists_from_spotify_playlist(playlist_link):
+def get_artists_from_spotify_playlist(playlist_link: str) -> List[Artist]:
     """
-    Retrieves a list of artists_list from a Spotify playlist.
+    Retrieves a list of artists from a Spotify playlist.
 
     This function uses the Spotify API to fetch the playlist tracks from the given 'playlist_link'.
-    It then processes the tracks and extracts the unique artists_list' names along with the number of songs by each artist.
+    It then processes the tracks and extracts the unique artists' names along with the number of songs by each artist.
 
-    Arguments:
+    Args:
         playlist_link (str): The link to the Spotify playlist.
 
     Returns:
         list: A list of 'Artist' objects representing each unique artist in the playlist.
     """
-    playlist = cut_content_after_question_mark(playlist_link)
-    # Extract the playlist ID from the link
-    playlist_id = playlist.split('/')[-1]
+    playlist_id = cut_content_after_question_mark(playlist_link).split('/')[-1]
 
     # Get the playlist tracks
     results = sp.playlist_items(playlist_id)
@@ -63,22 +57,32 @@ def get_artists_from_spotify_playlist(playlist_link):
         results = sp.next(results)
         tracks.extend(results.get('items'))
 
-    artists_list = []
+    # Create a dictionary to store artist song counts
     artist_song_count = {}
-
     for track in tracks:
-        for artist in track.get('track').get('artists'):
-            artist_name = artist.get('name')
-
-            if artist_name in artist_song_count:
-                artist_song_count[artist_name] += 1
-            else:
-                artist_song_count[artist_name] = 1
+        for artist in track["track"]["artists"]:
+            artist_name = artist["name"]
+            artist_song_count[artist_name] = artist_song_count.get(artist_name, 0) + 1
 
     # Create Artist objects with the collected data
+    artists_list = create_artists(artist_song_count)
+
+    return artists_list
+
+
+def create_artists(artist_song_count: dict) -> List[Artist]:
+    """
+    Creates a list of Artist objects from a dictionary of artist song counts.
+
+    Args:
+        artist_song_count (dict): A dictionary where the keys are artist names and the values are song counts.
+
+    Returns:
+        list: A list of 'Artist' objects with the corresponding song counts.
+    """
+    artists_list = []
     for artist_name, songs_num in artist_song_count.items():
         new_artist = Artist(name=artist_name, host_name_and_stage='none', weekend='none', date='none')
         new_artist.songs_num = songs_num
         artists_list.append(new_artist)
-
     return artists_list
