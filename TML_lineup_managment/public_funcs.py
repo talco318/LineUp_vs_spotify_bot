8763d75949cdb08d588f59cb5903c8040c49154d
tal <talcohen318@gmail.com>
2024-06-20 20:45:49 +0300

    use tomorrowland api to get lineup data online + img or artist added

--- TML_lineup_managment/public_funcs.py
+++ TML_lineup_managment/public_funcs.py
@@ -1,8 +1,12 @@
 import logging
 import json
+from urllib import response
+
 import requests
 import types
 from app.artist import Artist
+import csv
+import os
 
 tomorrowland_lineup_weekend_json_files = ['tml2024w1.json', 'tml2024w2.json']
 weekend_names = ["weekend 1", "weekend 2"]
@@ -12,7 +16,7 @@ def find_artist_and_update_new_data(artists_list: list[Artist], artist_name: str
                                     other_weekend: str, host_name_and_stage: str):
     """
     Search for an artist by name in the list of 'artists_list'.
-    If this artists already exist, update their data of the other show.
+    If artists already exist, update their data of the other show.
 
     Args:
         artists_list (List[Artist]): The list of artists to search in.
@@ -28,65 +32,70 @@ def find_artist_and_update_new_data(artists_list: list[Artist], artist_name: str
             artist.add_new_show(other_weekend, host_name_and_stage, new_date)
             break
 
+
 def extract_artists_from_tomorrowland_lineup() -> list[Artist]:
     """
-    Extract artist data from the Tomorrowland (TML) festival JSON lineup files.
+    Extract artist data from the Tomorrowland (TML) festival JSON lineup file.
 
     Returns:
-        List[Artist]: A list of 'Artist' objects containing the extracted data for the artists.
+    List[Artist]: A list of 'Artist' objects containing the extracted data for the artists.
     """
     artists: list[Artist] = []
+    url_w1 = 'https://artist-lineup-cdn.tomorrowland.com/TLBE24-W1-211903bb-da4c-445d-a1b3-6b17479a9fab.json'
+    url_w2 = 'https://artist-lineup-cdn.tomorrowland.com/TLBE24-W2-211903bb-da4c-445d-a1b3-6b17479a9fab.json'
 
-    for file in tomorrowland_lineup_weekend_json_files:
+    headers = {'User-Agent': 'My App 1.0'}
+    for url in [url_w1, url_w2]:
         try:
-            # Construct the URL for the JSON data
-            url = f'https://clashfinder.com/data/event/{file}'
-            headers = {'User-Agent': 'My App 1.0'}
-
             # Fetch the JSON data and parse it
             response = requests.get(url, headers=headers)
             response.raise_for_status()
             data = response.json()
 
             # Extract artist information from the JSON data
-            locations = data.get("locations")
-            for location in locations:
-                events = location.get("events")
-                for event in events:
-                    name = event.get("name")
-                    start = event.get("start")
-                    end = event.get("end")
-                    weekend = weekend_names[0] if file == 'tml2024w1.json' else weekend_names[1]
-                    host_name_and_stage = location.get("name")
-                    time = f'{start} to {end}'
+            performances = data.get("performances", [])
+            for performance in performances:
+                name = performance.get("name")
 
-                    # Create an 'Artist' object and update or add it to the artists list
-                    artist = Artist(name=name, host_name_and_stage=host_name_and_stage, weekend=weekend, date=time)
-                    matching_artists = [a for a in artists if a.name == artist.name]
-                    if matching_artists:
-                        find_artist_and_update_new_data(artists, artist.name, 0, time, weekend, host_name_and_stage)
-                    else:
-                        artists.append(artist)
+                stage = performance.get("stage", {}).get("name")
+                start_time = performance.get("startTime").split('+')[0]
+                end_time = performance.get("endTime").split('+')[0]
+                day = performance.get("day")
+                time = f"{day} {start_time} - {end_time}"
+                # Extract additional artist information if available
+                artist_info = performance.get("artists", [{}])[0]
+                image = artist_info.get("image", "")
+
+                weekend = weekend_names[0] if url == url_w1 else weekend_names[1]
+                artist = Artist(
+                    name=name,
+                    host_name_and_stage=stage,
+                    weekend=weekend,
+                    date=time,
+                    img=image if image else "",
+
+                )
+                matching_artists = [a for a in artists if a.name == artist.name]
+
+                if matching_artists:
+                    find_artist_and_update_new_data(artists, artist.name, 0, time, weekend, stage)
+                else:
+                    artists.append(artist)
 
         except requests.RequestException as e:
-            logging.error(f"Error fetching data for {file}: {str(e)}")
+            logging.error(f"Error fetching data: {str(e)}")
         except json.JSONDecodeError as e:
-            logging.error(f"Error decoding JSON in {file}: {str(e)}")
+            logging.error(f"Error decoding JSON: {str(e)}")
 
     return artists
 
-
-
-#for python anywhere:
-# import json
-# import os
-#
+# For local file lineup:
 # def extract_artists_from_tomorrowland_lineup() -> list[Artist]:
 #     """
-#     Extract artist data from local Tomorrowland (TML) festival JSON files in the parent directory.
+#     Extract artist data from the Tomorrowland (TML) festival local JSON lineup file.
 #
 #     Returns:
-#         List[Artist]: A list of 'Artist' objects containing the extracted data for the artists.
+#     List[Artist]: A list of 'Artist' objects containing the extracted data for the artists.
 #     """
 #     artists: list[Artist] = []
 #     weekend_names = ["Weekend 1", "Weekend 2"]  # Assuming weekend names are the same
@@ -101,28 +110,38 @@ def extract_artists_from_tomorrowland_lineup() -> list[Artist]:
 #                 data = json.load(jsonfile)
 #
 #                 # Extract artist information from the JSON data
-#                 locations = data.get("locations")
-#                 for location in locations:
-#                     events = location.get("events")
-#                     for event in events:
-#                         name = event.get("name")
-#                         start = event.get("start")
-#                         end = event.get("end")
-#                         weekend = weekend_names[0] if filename == "tml2024w1.json" else weekend_names[1]
-#                         host_name_and_stage = location.get("name")
-#                         time = f"{start} to {end}"
+#                 performances = data.get("performances", [])
+#                 for performance in performances:
+#                     name = performance.get("name")
+#                     stage = performance.get("stage", {}).get("name")
+#                     date = performance.get("date")
+#                     start_time = performance.get("startTime").split('+')[0]
+#                     end_time = performance.get("endTime").split('+')[0]
+#                     day = performance.get("day")
 #
-#                         # Create an 'Artist' object and update or add it to the artists list
-#                         artist = Artist(name=name, host_name_and_stage=host_name_and_stage, weekend=weekend, date=time)
-#                         matching_artists = [a for a in artists if a.name == artist.name]
-#                         if matching_artists:
-#                             find_artist_and_update_new_data(artists, artist.name, 0, time, weekend, host_name_and_stage)
-#                         else:
-#                             artists.append(artist)
+#                     # Extract additional artist information if available
+#                     artist_info = performance.get("artists", [{}])[0]
+#                     image = artist_info.get("image", "")
+#                     date = f"{day} {start_time} - {end_time}"
+#                     weekend = weekend_names[0] if filename == "tml2024w1.json" else weekend_names[1]
+#
+#                     # Create an 'Artist' object and add it to the artists list
+#                     artist = Artist(
+#                         name=name,
+#                         host_name_and_stage=stage,
+#                         weekend=weekend,
+#                         date=date
+#                     )
+#                     matching_artists = [a for a in artists if a.name == artist.name]
+#
+#                     if matching_artists:
+#                         find_artist_and_update_new_data(artists, artist.name, 0, date, weekend, stage)
+#                     else:
+#                         artists.append(artist)
 #
 #         except FileNotFoundError as e:
 #             logging.error(f"Error: File not found - {filepath}")
+#
 #         except json.JSONDecodeError as e:
 #             logging.error(f"Error decoding JSON in {filepath}: {str(e)}")
-#
 #     return artists
