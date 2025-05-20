424064b4f9d1d9f129677b6a713cc0de258b8214
tal <talcohen318@gmail.com>
2025-05-20 08:51:42 +0300

    changed to a new model of gemini

--- AI/AI_funcs_gemini.py
+++ AI/AI_funcs_gemini.py
@@ -48,7 +48,7 @@ def generate_response(artists_output: str, weekend_input: str):
             },
         ]
 
-        model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+        model = genai.GenerativeModel(model_name="gemini-2.0-flash",
                                       # generation_config=generation_config,
                                       safety_settings=safety_settings)
         prompt_parts = [
